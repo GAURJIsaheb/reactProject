d8b0212cc572cb4f536d1fb44304f0ea7a6e3d11
Aditya Gaur <127978919+GAURJIsaheb@users.noreply.github.com>
2026-02-23 12:28:05 +0530

    Update setup.js

--- server/setup.js
+++ server/setup.js
@@ -36,7 +36,7 @@ export function createServer() {
 
 
   app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.use(
     session({
       name: 'todo.sid',
